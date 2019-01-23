635b0f9dc9838762b597948b306c9fbfdd6b2b10
Jan Safranek <jsafrane@redhat.com>
2019-01-23 10:19:58 +0100

    Review updates

--- connection/connection.go
+++ connection/connection.go
@@ -1,8 +1,23 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package connection
 
 import (
 	"context"
-	"net"
 	"strings"
 	"time"
 
@@ -16,22 +31,21 @@ const (
 	connectionLoggingInterval = 10 * time.Second
 )
 
-// Connect opens insecure gRPC connection to a CSI driver. Address must be either absolute path to a socket file
-// or have format '<protocol>://', following gRPC name resolution mechanism at https://github.com/grpc/grpc/blob/master/doc/naming.md.
-// The function tries to connect indefinitely every second until it connects. The function automatically adds
-// interceptor for gRPC message logging.
-func Connect(address string, dialOptions ...grpc.DialOption) (*grpc.ClientConn, error) {
-	dialOptions = append(dialOptions,
+// Connect opens insecure gRPC connection to a CSI driver. Address must be either absolute path to UNIX domain socket
+// file or have format '<protocol>://', following gRPC name resolution mechanism at
+// https://github.com/grpc/grpc/blob/master/doc/naming.md.
+// The function tries to connect indefinitely every second until it connects. The function automatically disables TLS
+// and adds interceptor for logging of all gRPC messages at level 5.
+func Connect(address string) (*grpc.ClientConn, error) {
+	dialOptions := []grpc.DialOption{
 		grpc.WithInsecure(),                   // Don't use TLS, it's usually local Unix domain socket in a container.
 		grpc.WithBackoffMaxDelay(time.Second), // Retry every second after failure.
 		grpc.WithBlock(),                      // Block until connection succeeds.
 		grpc.WithUnaryInterceptor(LogGRPC),    // Log all messages.
-	)
+	}
 	if strings.HasPrefix(address, "/") {
 		// It looks like filesystem path.
-		dialOptions = append(dialOptions, grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
-			return net.DialTimeout("unix", addr, timeout)
-		}))
+		address = "unix://" + address
 	}
 	glog.Infof("Connecting to %s", address)
 
